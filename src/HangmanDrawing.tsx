18eef63e1756f593e82c4c1e88d0a895b9ba782b
Ech0ke <aidas.zygas@gmail.com>
2023-06-19 12:35:10 +0300

    Created hanging person styles

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./styles/HangmanDrawingStyle.css";
+
+const HEAD = <div className="drawing-head" />;
+const BODY = <div className="drawing-body" />;
+const RIGHTARM = <div className="drawing-right-arm" />;
+const LEFTARM = <div className="drawing-left-arm" />;
+const RIGHTLEG = <div className="drawing-right-leg" />;
+const LEFTLEG = <div className="drawing-left-leg" />;
+
 function HangmanDrawing() {
   return (
     <div className="drawing-wrapper">
+      {HEAD}
+      {BODY}
+      {RIGHTARM}
+      {LEFTARM}
+      {RIGHTLEG}
+      {LEFTLEG}
       <div className="drawing-rope" />
       <div className="drawing-side-pole" />
       <div className="drawing-pole" />
